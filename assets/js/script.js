7ea46e0d5a6234306e84eb7bb7a4b3b3205a008b
muditrajsade <132217112+muditrajsade@users.noreply.github.com>
2024-04-25 20:12:06 +0530

    modal_added

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,8 +37,56 @@ for (let i = 0; i < navElemArr.length; i++) {
   });
 }
 
+document.querySelector("#pro-btn").addEventListener("click",function(){
+  openModal();
+
+})
+
+function openModal() 
+{
+  var modal = document.getElementById("myModal");
+  document.body.style.overflowY='hidden';
+  modal.style.display = "block";
+  //event listener to close popup when clicking outside of it
+  window.addEventListener("click", outsideModalClick);
+}
+// close popup
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+
+  //remove event listener
+  window.removeEventListener("click", outsideModalClick);
+}
+
+//handles click outside of modal
+function outsideModalClick(event) 
+{
+  var modal = document.getElementById("myModal");
+  document.body.style.overflowY='visible';
+  if (event.target == modal) 
+  {
+      modal.style.display = "none";
+      window.removeEventListener("click", outsideModalClick);
+  }
+}
 
 
+
+document.querySelector("#sign_in").addEventListener("click",function(){
+
+  let rg = document.querySelector("#username").value;
+  let edf = document.querySelector("#password").value;
+  if(rg == 'harshit' && edf == "12345"){
+    closeModal();
+    alert("signed in");
+  }
+  else{
+    closeModal();
+    alert("invalid login credentials");
+  }
+
+});
 /**
  * header active state
  */
